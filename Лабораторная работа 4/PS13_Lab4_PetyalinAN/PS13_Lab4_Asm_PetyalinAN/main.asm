abaad781a22cc035275fbbfb689f642d1249b43a
gerdae <petyalin.a@mail.ru>
2026-04-02 13:52:38 +0300

    Add files via upload

---
+++ Лабораторная работа 4/PS13_Lab4_PetyalinAN/PS13_Lab4_Asm_PetyalinAN/main.asm
@@ -0,0 +1,39 @@
+;
+; PS13_Lab4_Asm_PetyalinAN.asm
+;
+; Created: 10/16/2025 2:28:04 PM
+; Author : g3rdae
+;
+
+
+reset:
+	rjmp main
+
+main:
+	sbi DDRD, PIND3
+	cbi DDRD, PIND2
+	sbi PORTD, PIND2
+
+loop:
+	sbic PIND, PIND2
+	rjmp led_off
+	sbi PORTD, PIND3 
+	rjmp setup_delay
+
+led_off:
+	cbi PORTD, PIND3
+
+setup_delay:
+	ldi r18, 0x52
+	ldi r24, 0x4E
+	ldi r25, 0x17
+
+loop_delay:
+	subi r18, 0x01
+	subi r24, 0x00
+	subi r25, 0x00
+	brne loop_delay
+	rjmp PC+0
+	rjmp PC+0
+	rjmp PC+0
+	rjmp loop
---
+++ Лабораторная работа 4/PS13_Lab4_PetyalinAN/PS13_Lab4_PetyalinAN/PS13_Lab4_PetyalinAN.c
@@ -0,0 +1,27 @@
+/*
+ * PS13_Lab4_PetyalinAN.c
+ *
+ * Created: 10/16/2025 2:18:51 PM
+ * Author : g3rdae
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+
+int main(void)
+{
+	DDRD |= (1 << PIND3);
+	DDRD &= ~ (1 << PIND2);
+	PORTD |= (1 << PIND2);
+	while (1)
+	{
+		if (PIND & (1 << PIND2))
+		PORTD &= ~(1 << PIND3);
+		else
+		PORTD |= (1 << PIND3);
+		_delay_ms(477);
+	}
+}
+
